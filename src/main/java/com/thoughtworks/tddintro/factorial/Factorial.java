9a5da9a1df117a2103845e5ffd9a4e99a1ebf82e
Adam J. Leuer <aleuer2@illinois.edu>
2016-02-23 09:38:05 -0600

    shouldReturnTwoWhenNumberIsTwo() now passes

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -1,10 +1,12 @@
 package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
+
     public Integer compute(int i) {
-        if (i == 1) {
-            return 1;
+        if ((i == 1) || (i == 2)) {
+            return i ;
         }
         return 0 ;
     }
+
 }
