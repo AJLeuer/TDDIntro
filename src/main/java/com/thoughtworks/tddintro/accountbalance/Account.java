60fad778bcd895534615958e173d4c334b93297a
Adam J. Leuer <aleuer2@illinois.edu>
2016-02-23 10:17:08 -0600

    shouldIncreaseMyBalanceWhenIDepositMoney()  now passes

---
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.tddintro.accountbalance;
+
+/**
+ * Created by tech on 2/23/2016.
+ */
+public class Account {
+
+    private int balance = 0 ;
+
+    public Account() {}
+
+    public void deposit(int amount) {
+        balance += amount ;
+    }
+
+    public int getBalance() {
+        return balance ;
+    }
+
+    public void withdraw(int i) {
+    }
+}
--- src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
@@ -1,18 +1,36 @@
 package com.thoughtworks.tddintro.accountbalance;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
-//
-//    @Test
-//    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-//
-//    }
-//
-//    @Test
-//    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
-//
-//    }
+
+    private Account account ;
+
+    @Before
+    public void setUp() {
+        account = new Account() ;
+    }
+
+    @Test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+        final int initialBalance = account.getBalance() ;
+        account.deposit(100);
+        Assert.assertTrue(account.getBalance() > initialBalance);
+    }
+
+    @Test
+    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
+        final int initialBalance = account.getBalance() ;
+        account.withdraw(50) ;
+        Assert.assertTrue(account.getBalance() < initialBalance);
+    }
+
+    @Test
+    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
+        final int initialBalance = account.getBalance() ;
+        account.withdraw(50) ;
+        Assert.assertFalse(account.getBalance() < initialBalance);
+    }
 }
